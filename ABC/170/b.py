42447da925845ba42ddae66fe68af1677f76bc0b
fumiyanll23 <02index04@gmail.com>
2020-09-27 16:51:24 +0900

    Update ABC170

--- ABC/170/b.py
+++ ABC/170/b.py
@@ -1,9 +1,12 @@
 X, Y = map(int, input().split())
 
-for i in range(50):
-  for j in range(50):
-    if(i+j == X and 2*i+4*j == Y):
-      print("Yes")
-print("No")
+flag = False
+for i in range(100):
+  for j in range(100):
+    if i+j == X and 2*i+4*j == Y:
+      flag = True
 
-# In case, print "Yes" and "No".
+if flag:
+  print("Yes")
+else:
+  print("No")
