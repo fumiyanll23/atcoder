f4656d0f6cbd64dbff1c97a9410d977e4794410c
fumiyanll23 <02index04@gmail.com>
2020-09-27 16:51:38 +0900

    Update ABC174

--- ABC/174/b.py
+++ ABC/174/b.py
@@ -1,9 +1,12 @@
 N, D = map(int, input().split())
-X, Y = list(map(int, input().split()))
+X = [0 for i in range(N)]
+Y = [0 for i in range(N)]
+for i in range(N):
+  X[i], Y[i] = map(int, input().split())
 cnt = 0
 
 for i in range(N):
-  if(D**2 >= (X[i]**2 + Y[i]**2)):
+  if D**2 >= (X[i]**2 + Y[i]**2):
     cnt += 1
 
 print(cnt)
