ca4181d3a6cb9ea51aae31a784bc19bdb62a9756
Fumiya Narita <02index04@gmail.com>
2020-10-15 23:15:48 +0900

    Update ABC174

---
+++ ABC/174/a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  // input
+  int X; scanf("%d", &X);
+
+  // output
+  if (X >= 30) {
+    puts("Yes");
+  }
+  else {
+    puts("No");
+  }
+
+  return 0;
+}
