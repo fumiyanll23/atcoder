79cc1d0cf32e3ca92644a6588d259850ba7af402
fumiyanll23 <02index04@gmail.com>
2020-11-16 00:56:27 +0900

    Add ABC83

--- ABC/83/b.py
+++ ABC/83/b.py
@@ -2,7 +2,7 @@ N, A, B = map(int, input().split())
 
 ans = 0
 for i in range(N+1):
-  ds = sum(list(map(int, str(i))))
-  if A<=ds and ds<=B:
+  dsum = sum(list(map(int, str(i))))
+  if A <= dsum <= B:
     ans += i
 print(ans)
