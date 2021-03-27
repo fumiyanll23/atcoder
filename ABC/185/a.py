bafa42964920a0deac541d6b4fef036e548b1bd9
fumiyanll23 <02index04@gmail.com>
2021-03-27 19:36:39 +0900

    Add ABC185

---
+++ ABC/185/a.py
@@ -0,0 +1,14 @@
+# ABC185A - ABC Preparation
+
+def main():
+    # input
+    As = list(map(int, input().split()))
+
+    # compute
+
+    # output
+    print(min(As))
+
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/185/a_other.py
@@ -0,0 +1,13 @@
+# ABC185A - ABC Preparation
+
+def main():
+    # input
+
+    # compute
+
+    # output
+    print(min(map(int, input().split())))
+
+
+if __name__ == '__main__':
+    main()
