ec326c1ee4856cc6d6848d5a2bd1f7dcbf7215b9
fumiyanll23 <02index04@gmail.com>
2021-03-27 19:36:48 +0900

    Add ABC075

---
+++ ABC/075/a.py
@@ -0,0 +1,16 @@
+def main():
+    # input
+    A, B, C = map(int, input().split())
+
+    # compute
+
+    # output
+    if A == B:
+        print(C)
+    elif B == C:
+        print(A)
+    else:
+        print(B)
+
+if __name__ == '__main__':
+    main()
