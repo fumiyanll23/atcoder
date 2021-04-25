252a0ad6baacdba271a014b24485a931dcb89f21
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-04-25 19:56:23 +0900

    Add DDCC/2020/b.py

---
+++ others/DDCC/2020/b.py
@@ -0,0 +1,19 @@
+def main():
+    # input
+    N = int(input())
+    As = list(map(int, input().split()))
+
+    # compute
+    diffs = [0] * (N-1)
+    r, s = 0, sum(As)
+    for i in range(N-1):
+        r += As[i]
+        s -= As[i]
+        diffs[i] = abs(s-r)
+
+    # output
+    print(max(0, min(diffs)))
+
+
+if __name__ == '__main__':
+    main()
