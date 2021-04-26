b6d42003ba69f8bc2114a1638a8dd99320de3d96
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-04-26 13:23:36 +0900

    Update ABC/128/b.py

--- ABC/128/b.py
+++ ABC/128/b.py
@@ -1,9 +1,24 @@
-import numpy as np
+def main():
+  # input
+  N = int(input())
+  SPs = [list(input().split()) for _ in range(N)]
+  for i in range(N):
+    SPs[i][1] = (-1) * int(SPs[i][1])
 
-N = int(input())
-rest = []
-for i in range(N):
-  rest.append(list(map(str, input().split())))
-  rest[i][1] = int(rest[i][1])
-print(N)
-print(rest)
+  # compute
+  SPs_sorted = sorted(SPs, key=lambda x: (x[0], x[1]))
+  for i in range(N):
+    SPs_sorted[i][1] = str((-1)*SPs_sorted[i][1])
+  name_and_number = []
+  for i in range(N):
+    name_and_number.append([SPs_sorted[i][0], SPs_sorted[i][1], i])
+
+  # output
+  for name,points in SPs:
+    for i in range(N):
+      if name==name_and_number[i][0] and points==name_and_number[i][1]:
+        print(name_and_number[i][2]+1)
+
+
+if __name__ == '__main__':
+  main()
