afc883909ee18b23f120547addbe9a65285fcc8d
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-05-11 22:21:33 +0900

    Add ABC/184/a.py

---
+++ ABC/184/a.py
@@ -0,0 +1,13 @@
+def main():
+    # input
+    a, b = map(int, input().split())
+    c, d = map(int, input().split())
+
+    # compute
+
+    # output
+    print(a*d - b*c)
+
+
+if __name__ == '__main__':
+    main()
