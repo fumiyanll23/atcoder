57ace34e5661002770fcf2f0404e217532a0c2fb
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-01 22:00:33 +0900

    Add ABC/195/a.py

---
+++ ABC/195/a.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    M, H = map(int, input().split())
+
+    # compute
+
+    # output
+    if H % M:
+        print('No')
+    else:
+        print('Yes')
+
+
+if __name__ == '__main__':
+    main()
