c3775a08d403cc289c30fa346d7d8d76119496e7
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-10 00:03:50 +0900

    Add ABC/016

---
+++ ABC/016/a.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    M, D = map(int, input().split())
+
+    # compute
+
+    # output
+    if M % D:
+        print('NO')
+    else:
+        print('YES')
+
+
+if __name__ == '__main__':
+    main()
