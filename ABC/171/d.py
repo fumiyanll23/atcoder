31a64501bd17be93c7b5939ed585f4e0b3d689c8
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-09 17:13:29 +0900

    Update ABC/171/d.py

--- ABC/171/d.py
+++ ABC/171/d.py
@@ -1,10 +1,23 @@
-N = int(input())
-A = list(map(int, input().split()))
-Q = int(input())
-B, C = map(int, input().split())
+def main():
+    # input
+    N = int(input())
+    As = [*map(int, input().split())]
+    Q = int(input())
+    BCs = [[*map(int, input().split())] for _ in range(Q)]
 
-A = sorted(A) # probably faster if add "break"
-for i in range(Q):
-  for j in range(N):
-    if(A[j] == B[i]):
-      A[j] = C[i]
+    # compute
+    cnts = [0] * 10**5
+    for A in As:
+        cnts[A-1] += 1
+    ans = sum(As)
+    for B,C in BCs:
+        ans += (C-B) * cnts[B-1]
+        cnts[C-1] += cnts[B-1]
+        cnts[B-1] = 0
+        print(ans)
+
+    # output
+
+
+if __name__ == '__main__':
+    main()
