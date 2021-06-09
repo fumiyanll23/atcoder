a8e334f9bb9663255764954997f4a586f3f7e9dd
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-10 00:04:58 +0900

    Add ABC/025/

---
+++ ABC/025/a.py
@@ -0,0 +1,13 @@
+from itertools import product
+
+def main():
+    # input
+
+    # compute
+
+    # output
+    print(''.join([*product(input(), repeat=2)][int(input())-1]))
+
+
+if __name__ == '__main__':
+    main()
