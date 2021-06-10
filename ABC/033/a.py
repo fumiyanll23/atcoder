9852f45ae2127155cca9abcc1674eb0c3db4f5a2
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-10 21:07:13 +0900

    Add ABC/033/

---
+++ ABC/033/a.py
@@ -0,0 +1,11 @@
+def main():
+    # input
+
+    # compute
+
+    # output
+    print('SAME' if len(set(input()))==1 else 'DIFFERENT')
+
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/033/a_ans.py
@@ -0,0 +1,11 @@
+def main():
+    # input
+
+    # compute
+
+    # output
+    print('DIFFERENT' if int(input())%1111 else 'SAME')
+
+
+if __name__ == '__main__':
+    main()
