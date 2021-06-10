c447a59d88e7f10b63a8c89e8bdb6c0a641e2e6b
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-10 21:07:29 +0900

    Add ABC/036/

---
+++ ABC/036/a.py
@@ -0,0 +1,14 @@
+from math import ceil
+
+def main():
+    # input
+    A, B = map(int, input().split())
+
+    # compute
+
+    # output
+    print(ceil(B/A))
+
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/036/a_ans.py
@@ -0,0 +1,12 @@
+def main():
+    # input
+    A, B = map(int, input().split())
+
+    # compute
+
+    # output
+    print((B+A-1) // A)
+
+
+if __name__ == '__main__':
+    main()
