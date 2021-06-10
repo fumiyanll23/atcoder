d5f4e37ca759c4bc9ce5dcfe0af565d220024904
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-10 21:08:05 +0900

    Add ABC/041/

---
+++ ABC/041/a.py
@@ -0,0 +1,13 @@
+def main():
+    # input
+    s = input()
+    i = int(input())
+
+    # compute
+
+    # output
+    print(s[i-1])
+
+
+if __name__ == '__main__':
+    main()
---
+++ ABC/041/a_1.py
@@ -0,0 +1,11 @@
+def main():
+    # input
+
+    # compute
+
+    # output
+    print(input()[int(input())-1])
+
+
+if __name__ == '__main__':
+    main()
