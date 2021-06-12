687a44c82eb64fad1c08735af4135183557ebd7a
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-12 20:49:51 +0900

    Add ABC/058/

---
+++ ABC/058/a.py
@@ -0,0 +1,12 @@
+def main():
+    # input
+    a, b, c = map(int, input().split())
+
+    # compute
+
+    # output
+    print('YES' if b-a==c-b else 'NO')
+
+
+if __name__ == '__main__':
+    main()
