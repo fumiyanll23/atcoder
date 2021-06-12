51282acd9bf22dbb9d8d75d29c6028039a3d0b33
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-12 21:12:12 +0900

    Add ABC/061/a.py

---
+++ ABC/061/a.py
@@ -0,0 +1,12 @@
+def main():
+    # input
+    A, B, C = map(int, input().split())
+
+    # compute
+
+    # output
+    print('Yes' if A<=C<=B else 'No')
+
+
+if __name__ == '__main__':
+    main()
