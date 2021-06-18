44acb964cad97989dae96f8a1de0f6a684daa216
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-18 22:45:41 +0900

    Add ABC/073/a.py

---
+++ ABC/073/a.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    N = int(input())
+
+    # compute
+
+    # output
+    if N//10==9 or N%10==9:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
