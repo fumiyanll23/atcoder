03e57bee7ac79f41fa1fecfb2af4e017eaf4759b
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-06-22 19:15:20 +0900

    Add ABC/034/b*.py

---
+++ ABC/034/b.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    n = int(input())
+
+    # compute
+
+    # output
+    if n%2 == 0:
+        print(n - 1)
+    else:
+        print(n + 1)
+
+
+if __name__ == '__main__':
+    main()
