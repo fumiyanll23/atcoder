8200ee5311e546831b439473dcbd361f94e5668e
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-07-11 20:02:00 +0900

    add: 020.py

---
+++ others/typical90/020.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    a, b, c = map(int, input().split())
+
+    # compute
+
+    # output
+    if pow(c, b) > a:
+        print('Yes')
+    else:
+        print('No')
+
+
+if __name__ == '__main__':
+    main()
