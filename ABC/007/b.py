dfd6da3e0ce6c14131632f82401f9fa1e5d27fe2
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-07-18 07:01:53 +0900

    add: ABC/007/b.py

---
+++ ABC/007/b.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    A = input()
+
+    # compute
+
+    # output
+    if A == 'a':
+        print(-1)
+    else:
+        print('a')
+
+
+if __name__ == '__main__':
+    main()
