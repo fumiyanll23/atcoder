9363b69d199d60e7ec737f609b68b5332fb9bde6
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-07-22 18:55:33 +0900

    add: ABC/060/a.py

---
+++ ABC/060/a.py
@@ -0,0 +1,15 @@
+def main():
+    # input
+    A, B, C = input().split()
+
+    # compute
+
+    # output
+    if A[-1]==B[0] and B[-1]==C[0]:
+        print('YES')
+    else:
+        print('NO')
+
+
+if __name__ == '__main__':
+    main()
