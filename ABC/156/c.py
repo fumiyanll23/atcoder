2f28720a3ec9ffe3346322fa5ce6261cbf08563e
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-07-30 15:12:28 +0900

    update: ABC/156/c.py

--- ABC/156/c.py
+++ ABC/156/c.py
@@ -1,9 +1,19 @@
-N = int(input())
-X = list(map(int, input().split()))
+def main():
+  # input
+  N = int(input())
+  Xs = [*map(int, input().split())]
 
-X = sorted(X)
-d = [0 for i in range(N)]
-for i in range(1, max(X)):
-  for j in range(N):
-    d[j] = (X[j]-i) ** 2
-print(min(d))
+  # compute
+  ans = float('inf')
+  for p in range(min(Xs), max(Xs)+1):
+    dist = 0
+    for X in Xs:
+      dist += (X - p)**2
+    ans = min(ans, dist)
+
+  # output
+  print(ans)
+
+
+if __name__ == '__main__':
+  main()
