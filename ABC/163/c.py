2d9174ce121b4718e972d79b9aaf302a6c7424de
fumiyanll23 <fumiyanll23.public@gmail.com>
2021-08-01 19:33:53 +0900

    update: ABC/163/c.py

--- ABC/163/c.py
+++ ABC/163/c.py
@@ -1,12 +1,17 @@
-N = int(input())
-A = list(map(int, input().split()))
+def main():
+  # input
+  N = int(input())
+  As = [*map(int, input().split())]
 
-sub= [0 for i in range(N)] # Number of sub of No. i
-for i in range(N-1):
-  for j in range(N):
-    if(A[i]-1 == j):
-      sub[j] += 1
-for i in range(N):
-  print(sub[i])
+  # compute
+  cnts = [0] * N
+  for A in As:
+    cnts[A-1] += 1
 
-# TLE & WA
+  # output
+  for cnt in cnts:
+    print(cnt)
+
+
+if __name__ == '__main__':
+  main()
