37fabe578c60dba39d5f77b785faf1b8f7869dc3
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-04-16 13:13:04 +0900

    Add: ABC/049/a.cpp

---
+++ ABC/049/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // input
+  char c;
+  cin >> c;
+
+  // compute
+
+  // output
+  if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+    cout << "vowel" << endl;
+  }
+  else {
+    cout << "consonant" << endl;
+  }
+}
